1e570dc9d6a17f1b5150559ec9f391d3d77bf41b
LimitToInfinity <21stLimitless@gmail.com>
2020-04-22 18:29:47 -0600

    Adds socket.io-p2p package.

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const httpServer = app.listen(port, () => console.log(`listening on port ${port}
 
 const socketio = require('socket.io');
 const io = socketio(httpServer);
+const p2p = require('socket.io-p2p-server').Server;
+io.use(p2p);
+
 
 app.use(express.static(__dirname + '/videoChat'));
 app.get('/', (res, req) => res.render('index.html'));
@@ -14,9 +17,6 @@ io.on('connection', handleSocket);
 function handleSocket(socket) {
     socket.on('open', (message) => console.log(message))
     socket.emit('welcome', 'Hello Human')
-    socket.on('offer', (offer, offerPC) => {
-        console.log(offerPC)
-        socket.emit('offer', offer, offerPC)
-    });
+    socket.on('offer', (offer, offerPC) => socket.emit('offer', offer, offerPC));
     socket.on('answer', (answer, answerPC) => socket.emit('answer', answer, answerPC))
 }
